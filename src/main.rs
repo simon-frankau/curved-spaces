bf2ed38bce31d642ead2aa9641200273e1c72d2e
Simon Frankau <sgf@arbitrary.name>
2024-04-26 21:46:54 +0100

    Unify the winit using back-ends - glutin_winit and wasm32

--- src/main.rs
+++ src/main.rs
@@ -1,160 +1,36 @@
 //
 // TODO: Initial GL-displaying thing.
 //
-// Based on
-// https://github.com/grovesNL/glow/blob/main/examples/hello/src/main.rs
-//
 use anyhow::*;
 use glow::{Context, *};
 
 ////////////////////////////////////////////////////////////////////////
-// wasm32: Create a context from a WebGL2 context on wasm32 targets.
+// winit: Shared between wasm32 and glutin_winit.
 //
 
-#[cfg(target_arch = "wasm32")]
-type Program = WebProgramKey;
-
-#[cfg(target_arch = "wasm32")]
+#[cfg(any(target_arch = "wasm32", feature = "glutin_winit"))]
 #[derive(Debug)]
 pub enum UserEvent {
     Redraw(std::time::Duration),
 }
 
-#[cfg(target_arch = "wasm32")]
+#[cfg(any(target_arch = "wasm32", feature = "glutin_winit"))]
 struct Platform {
     gl: std::sync::Arc<Context>,
     shader_version: &'static str,
     window: winit::window::Window,
     event_loop: Option<winit::event_loop::EventLoop<UserEvent>>,
+
+    #[cfg(feature = "glutin_winit")]
+    gl_surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
+    #[cfg(feature = "glutin_winit")]
+    gl_context: glutin::context::PossiblyCurrentContext,
 }
 
-#[cfg(target_arch = "wasm32")]
-// Taken from eframe
-pub fn native_pixels_per_point() -> f32 {
-    // TODO: Take a Window
-    let pixels_per_point = web_sys::window().unwrap().device_pixel_ratio() as f32;
-    if pixels_per_point > 0.0 && pixels_per_point.is_finite() {
-        pixels_per_point
-    } else {
-        1.0
-    }
-}
 
-#[cfg(target_arch = "wasm32")]
-impl Platform {
-    fn new() -> Result<Platform> {
-        // TODO: Make parameters?
-        let width = 1024;
-        let height = 768;
-
-        use wasm_bindgen::JsCast;
-        use winit::platform::web::WindowBuilderExtWebSys;
-        use winit::platform::web::WindowExtWebSys;
-
-        let event_loop =
-            winit::event_loop::EventLoopBuilder::<UserEvent>::with_user_event().build()?;
-        let window;
-
-        if cfg!(with_canvas) {
-            // Use existing <canvas/> element.
-            let canvas_id = "canvas";
-            let canvas = web_sys::window()
-                .ok_or_else(|| anyhow!("Couldn't get window"))?
-                .document()
-                .ok_or_else(|| anyhow!("Couldn't get document"))?
-                .dyn_into::<web_sys::HtmlCanvasElement>()
-                .map_err(|_| anyhow!("Couldn't cast to canvas"))?;
-
-            let nppp = native_pixels_per_point();
-            canvas.set_width((width as f32 * nppp) as u32);
-            canvas.set_height((height as f32 * nppp) as u32);
-
-            window = winit::window::WindowBuilder::new()
-                .with_inner_size(winit::dpi::LogicalSize::new(width, height))
-                .with_canvas(Some(canvas.clone()))
-                .build(&event_loop)?;
-        } else {
-            // Insert <canvas/> element under given element.
-            window = winit::window::WindowBuilder::new()
-                .with_inner_size(winit::dpi::LogicalSize::new(width, height))
-                .build(&event_loop)?;
-
-            let doc = web_sys::window()
-                .ok_or_else(|| anyhow!("Couldn't get window"))?
-                .document()
-                .ok_or_else(|| anyhow!("Couldn't get document"))?;
-
-            let canvas = window
-                .canvas()
-                .ok_or_else(|| anyhow!("Couldn't get canvas"))?;
-
-            let nppp = native_pixels_per_point();
-            canvas.set_width((width as f32 * nppp) as u32);
-            canvas.set_height((height as f32 * nppp) as u32);
-            canvas
-                .set_attribute(
-                    "style",
-                    &format!("width: {}px; height: {}px;", width, height),
-                )
-                .map_err(|_| anyhow!("Couldn't set style on canvas"))?;
-            let canvas_elt = web_sys::Element::from(canvas);
-            let dst = doc
-                .get_element_by_id("wasm-canvas")
-                .ok_or_else(|| anyhow!("Couldn't get element '{}'", "wasm-canvas"))?;
-            dst.append_child(&canvas_elt)
-                .map_err(|_| anyhow!("Couldn't add canvas to HTML"))?;
-        }
-
-        let canvas = window
-            .canvas()
-            .ok_or_else(|| anyhow!("Couldn't get canvas"))?;
-        let webgl2_context = canvas
-            .get_context("webgl2")
-            .map_err(|_| anyhow!("Couldn't get webgl2 context"))?
-            .ok_or_else(|| anyhow!("Couldn't get webgl2 context"))?
-            .dyn_into::<web_sys::WebGl2RenderingContext>()
-            .map_err(|_| anyhow!("Couldn't cast to WebGL2RenderingContext"))?;
-        let gl = glow::Context::from_webgl2_context(webgl2_context);
-        Ok(Platform {
-            gl: std::sync::Arc::new(gl),
-            shader_version: "#version 300 es",
-            window,
-            event_loop: Some(event_loop),
-        })
-    }
-
-    fn swap_buffers(&self) {
-        // Not needed on wasm, as automatically
-        // switches when the function returns.
-        // TODO
-        // self.gl_surface.swap_buffers(&self.gl_context).unwrap();
-        // self.window.set_visible(true);
-    }
-
-    fn resize(&self, physical_size: &winit::dpi::PhysicalSize<u32>) {
-        // TODO: wasm equivalent?
-        /*
-                self.gl_surface.resize(
-                    &self.gl_context,
-                    physical_size.width.try_into().unwrap(),
-                    physical_size.height.try_into().unwrap(),
-                );
-        }
-        */
-    }
-
-    fn run_event_loop(
-        event_loop: winit::event_loop::EventLoop<UserEvent>,
-        event_fn: impl FnMut(
-                winit::event::Event<UserEvent>,
-                &winit::event_loop::EventLoopWindowTarget<UserEvent>,
-            ) + 'static,
-    ) {
-        // TODO event_loop.run(event_fn);
-        use winit::platform::web::EventLoopExtWebSys;
-        event_loop.spawn(event_fn);
-    }
-
+#[cfg(any(target_arch = "wasm32", feature = "glutin_winit"))]
+impl Platform
+{
     // TODO: Still need to manage web scaling changing.
     fn run(mut self, drawable: Drawable) {
         // `run` "uses up" the event_loop, so we move it out.
@@ -274,28 +150,135 @@ impl Platform {
 }
 
 ////////////////////////////////////////////////////////////////////////
+// wasm32: Create a context from a WebGL2 context on wasm32 targets.
+//
+
+#[cfg(target_arch = "wasm32")]
+type Program = WebProgramKey;
+
+#[cfg(target_arch = "wasm32")]
+// Taken from eframe
+pub fn native_pixels_per_point() -> f32 {
+    // TODO: Take a Window
+    let pixels_per_point = web_sys::window().unwrap().device_pixel_ratio() as f32;
+    if pixels_per_point > 0.0 && pixels_per_point.is_finite() {
+        pixels_per_point
+    } else {
+        1.0
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+impl Platform {
+    fn new() -> Result<Platform> {
+        // TODO: Make parameters?
+        let width = 1024;
+        let height = 768;
+
+        use wasm_bindgen::JsCast;
+        use winit::platform::web::WindowBuilderExtWebSys;
+        use winit::platform::web::WindowExtWebSys;
+
+        let event_loop =
+            winit::event_loop::EventLoopBuilder::<UserEvent>::with_user_event().build()?;
+        let window;
+
+        if cfg!(with_canvas) {
+            // Use existing <canvas/> element.
+            let canvas_id = "canvas";
+            let canvas = web_sys::window()
+                .ok_or_else(|| anyhow!("Couldn't get window"))?
+                .document()
+                .ok_or_else(|| anyhow!("Couldn't get document"))?
+                .dyn_into::<web_sys::HtmlCanvasElement>()
+                .map_err(|_| anyhow!("Couldn't cast to canvas"))?;
+
+            let nppp = native_pixels_per_point();
+            canvas.set_width((width as f32 * nppp) as u32);
+            canvas.set_height((height as f32 * nppp) as u32);
+
+            window = winit::window::WindowBuilder::new()
+                .with_inner_size(winit::dpi::LogicalSize::new(width, height))
+                .with_canvas(Some(canvas.clone()))
+                .build(&event_loop)?;
+        } else {
+            // Insert <canvas/> element under given element.
+            window = winit::window::WindowBuilder::new()
+                .with_inner_size(winit::dpi::LogicalSize::new(width, height))
+                .build(&event_loop)?;
+
+            let doc = web_sys::window()
+                .ok_or_else(|| anyhow!("Couldn't get window"))?
+                .document()
+                .ok_or_else(|| anyhow!("Couldn't get document"))?;
+
+            let canvas = window
+                .canvas()
+                .ok_or_else(|| anyhow!("Couldn't get canvas"))?;
+
+            let nppp = native_pixels_per_point();
+            canvas.set_width((width as f32 * nppp) as u32);
+            canvas.set_height((height as f32 * nppp) as u32);
+            canvas
+                .set_attribute(
+                    "style",
+                    &format!("width: {}px; height: {}px;", width, height),
+                )
+                .map_err(|_| anyhow!("Couldn't set style on canvas"))?;
+            let canvas_elt = web_sys::Element::from(canvas);
+            let dst = doc
+                .get_element_by_id("wasm-canvas")
+                .ok_or_else(|| anyhow!("Couldn't get element '{}'", "wasm-canvas"))?;
+            dst.append_child(&canvas_elt)
+                .map_err(|_| anyhow!("Couldn't add canvas to HTML"))?;
+        }
+
+        let canvas = window
+            .canvas()
+            .ok_or_else(|| anyhow!("Couldn't get canvas"))?;
+        let webgl2_context = canvas
+            .get_context("webgl2")
+            .map_err(|_| anyhow!("Couldn't get webgl2 context"))?
+            .ok_or_else(|| anyhow!("Couldn't get webgl2 context"))?
+            .dyn_into::<web_sys::WebGl2RenderingContext>()
+            .map_err(|_| anyhow!("Couldn't cast to WebGL2RenderingContext"))?;
+        let gl = glow::Context::from_webgl2_context(webgl2_context);
+        Ok(Platform {
+            gl: std::sync::Arc::new(gl),
+            shader_version: "#version 300 es",
+            window,
+            event_loop: Some(event_loop),
+        })
+    }
+
+    fn swap_buffers(&self) {
+        // Not needed on wasm, as automatically
+        // switches when the function returns.
+    }
+
+    fn resize(&self, physical_size: &winit::dpi::PhysicalSize<u32>) {
+        // TODO: wasm equivalent?
+    }
+
+    fn run_event_loop(
+        event_loop: winit::event_loop::EventLoop<UserEvent>,
+        event_fn: impl FnMut(
+                winit::event::Event<UserEvent>,
+                &winit::event_loop::EventLoopWindowTarget<UserEvent>,
+            ) + 'static,
+    ) {
+        use winit::platform::web::EventLoopExtWebSys;
+        event_loop.spawn(event_fn);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
 // glutin_winit: Create a context from a glutin window on non-wasm32
 // targets.
 //
 
 #[cfg(feature = "glutin_winit")]
 type Program = NativeProgram;
-
-#[cfg(feature = "glutin_winit")]
-#[derive(Debug)]
-pub enum UserEvent {
-    Redraw(std::time::Duration),
-}
-
-#[cfg(feature = "glutin_winit")]
-struct Platform {
-    gl: std::sync::Arc<Context>,
-    gl_surface: glutin::surface::Surface<glutin::surface::WindowSurface>,
-    gl_context: glutin::context::PossiblyCurrentContext,
-    shader_version: &'static str,
-    window: winit::window::Window,
-    event_loop: Option<winit::event_loop::EventLoop<UserEvent>>,
-}
 
 #[cfg(feature = "glutin_winit")]
 impl Platform {
@@ -396,123 +379,6 @@ impl Platform {
             ) + 'static,
     ) {
         let _ = event_loop.run(event_fn);
-    }
-
-    // TODO: Still need to manage web scaling changing.
-    fn run(mut self, drawable: Drawable) {
-        // `run` "uses up" the event_loop, so we move it out.
-        let mut event_loop = None;
-        std::mem::swap(&mut event_loop, &mut self.event_loop);
-        let event_loop = event_loop.expect("Event loop already run");
-
-        let mut egui_glow =
-            egui_glow::winit::EguiGlow::new(&event_loop, self.gl.clone(), None, None);
-
-        let event_loop_proxy = egui::mutex::Mutex::new(event_loop.create_proxy());
-        egui_glow
-            .egui_ctx
-            .set_request_repaint_callback(move |info| {
-                event_loop_proxy
-                    .lock()
-                    .send_event(UserEvent::Redraw(info.delay))
-                    .expect("Cannot send event");
-            });
-
-        let mut repaint_delay = std::time::Duration::MAX;
-
-        let event_fn =
-            move |event,
-                  event_loop_window_target: &winit::event_loop::EventLoopWindowTarget<
-                UserEvent,
-            >| {
-                let mut redraw = || {
-                    let mut quit = false;
-
-                    egui_glow.run(&self.window, |egui_ctx| {
-                        egui::SidePanel::left("my_side_panel").show(egui_ctx, |ui| {
-                            ui.heading("Hello World!");
-                            if ui.button("Quit").clicked() {
-                                quit = true;
-                            }
-                            // TODO ui.color_edit_button_rgb(&mut clear_color);
-                        });
-                    });
-
-                    if quit {
-                        event_loop_window_target.exit();
-                    } else {
-                        event_loop_window_target.set_control_flow(if repaint_delay.is_zero() {
-                            self.window.request_redraw();
-                            winit::event_loop::ControlFlow::Poll
-                        } else if let Some(repaint_after_instant) =
-                            web_time::Instant::now().checked_add(repaint_delay)
-                        {
-                            winit::event_loop::ControlFlow::WaitUntil(repaint_after_instant)
-                        } else {
-                            winit::event_loop::ControlFlow::Wait
-                        });
-                    }
-
-                    {
-                        unsafe {
-                            use glow::HasContext as _;
-                            // self.gl.clear_color(clear_color[0], clear_color[1], clear_color[2], 1.0);
-                            self.gl.clear(glow::COLOR_BUFFER_BIT);
-                        }
-
-                        // draw things behind egui here
-                        drawable.draw(&self.gl);
-
-                        egui_glow.paint(&self.window);
-
-                        // draw things on top of egui here
-
-                        self.swap_buffers();
-                    }
-                };
-
-                match event {
-                    winit::event::Event::WindowEvent { event, .. } => {
-                        use winit::event::WindowEvent;
-                        if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
-                            event_loop_window_target.exit();
-                            return;
-                        }
-
-                        if matches!(event, WindowEvent::RedrawRequested) {
-                            redraw();
-                            return;
-                        }
-
-                        if let winit::event::WindowEvent::Resized(physical_size) = &event {
-                            self.resize(physical_size);
-                        }
-
-                        let event_response = egui_glow.on_window_event(&self.window, &event);
-
-                        if event_response.repaint {
-                            self.window.request_redraw();
-                        }
-                    }
-
-                    winit::event::Event::UserEvent(UserEvent::Redraw(delay)) => {
-                        repaint_delay = delay;
-                    }
-                    winit::event::Event::LoopExiting => {
-                        egui_glow.destroy();
-                        drawable.close(&self.gl);
-                    }
-                    winit::event::Event::NewEvents(
-                        winit::event::StartCause::ResumeTimeReached { .. },
-                    ) => {
-                        self.window.request_redraw();
-                    }
-
-                    _ => (),
-                }
-            };
-
-        Self::run_event_loop(event_loop, event_fn);
     }
 }
 
